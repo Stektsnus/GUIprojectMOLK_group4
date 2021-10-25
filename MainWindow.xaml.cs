30f769fd696123f96ca8e42d88199f69d5011a99
Elmabt <elmabristric20@gmail.com>
2021-10-25 14:51:05 +0200

    clean c# sheet new xaml code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,55 +23,6 @@ namespace GUIprojectMOLK_group4
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Process process = new Process();
-
-
-            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\Molk";
-            var startInfo = new ProcessStartInfo
-            {
-                WorkingDirectory = path,
-                WindowStyle = ProcessWindowStyle.Normal,
-                FileName = "cmd.exe",
-                RedirectStandardInput = true,
-                CreateNoWindow = true,
-                UseShellExecute = false
-            };
-
-            process.StartInfo = startInfo;
-            process.Start();
-
-            string commandString = $"molk -j \"{destinationBox.Text}\\archive2.molk\" \"{fileBox.Text}\"";
-
-            process.StandardInput.WriteLine(commandString);
-            
-        }
-
-        private void destinationButton_Click(object sender, RoutedEventArgs e)
-        {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
-            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                destinationBox.Text = folderBrowserDialog.SelectedPath;
-            }
-        }
-        private void fileButton_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = @"C:\";
-            openFileDialog.RestoreDirectory = true;
-            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                string fileSelected = openFileDialog.FileName;
-                fileBox.Text = fileSelected;
-            }
-        }
+     
     }
 }
